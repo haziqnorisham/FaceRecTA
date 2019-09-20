094b684924aa7b2698ca7c11dfa7c1c9b3d81223
Haziq <haziqnorisham@protonmail.com>
2019-09-20 11:24:26 +0800

    added registration page for superuser

--- administrator/urls.py
+++ administrator/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='administrator_home'),
+    path('registration/', views.registration, name='registration'),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 @login_required
 def home(requests):
     return render(requests, "administrator/administrator_home.html")
+
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def registration(requests):
+    return render(requests, "administrator/registration.html")
