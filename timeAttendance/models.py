07ef747b43b5151cf1746f32eda8f9a8cc48ce96
Haziq <haziqnorisham@protonmail.com>
2019-09-22 13:35:32 +0800

    add EmployeeDetails model

--- timeAttendance/models.py
+++ timeAttendance/models.py
@@ -6,3 +6,9 @@ class EmployeeAttendance(models.Model):
     name = models.CharField(max_length=32)
     capture_time = models.CharField(max_length=200)
     capture_location = models.CharField(max_length=100, null=True, blank=True)
+
+class EmployeeDetail(models.Model):
+    employee_id = models.IntegerField()
+    name = models.CharField(max_length=32)
+    Gender = models.IntegerField()
+    CustomizeID = models.IntegerField(unique=True)
