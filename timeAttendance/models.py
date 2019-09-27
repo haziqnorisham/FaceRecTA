f639b4b7123a62ce0a97b47da7ca69d48e4dedd9
Haziq <haziqnorisham@protonmail.com>
2019-09-28 00:19:13 +0800

    added foreign key to employee attendance models

--- timeAttendance/models.py
+++ timeAttendance/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class EmployeeAttendance(models.Model):
-    employee_id = models.IntegerField()
-    name = models.CharField(max_length=32)
-    capture_time = models.CharField(max_length=200)
-    capture_location = models.CharField(max_length=100, null=True, blank=True)
 
 class EmployeeDetail(models.Model):
     employee_id = models.IntegerField()
@@ -15,6 +10,13 @@ class EmployeeDetail(models.Model):
     img_name = models.CharField(max_length=500, null=True)
     department = models.CharField(max_length=500, null=True)
     branch = models.CharField(max_length=500, null=True)
+
+class EmployeeAttendance(models.Model):
+    employee_id = models.IntegerField()
+    name = models.CharField(max_length=32)
+    capture_time = models.CharField(max_length=200)
+    capture_location = models.CharField(max_length=100, null=True, blank=True)
+    EmployeeDetail = models.ForeignKey(EmployeeDetail, on_delete=models.CASCADE, null=True)
 
 class TerminalDetails(models.Model):
     terminal_id = models.IntegerField(primary_key=True)
